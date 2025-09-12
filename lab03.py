80f9f016e4d57c64f134e56611f316dfe042ab7b
ssamps2 <ssamps2@clemson.edu>
2025-09-12 16:18:55 -0400

    initialize repo

---
+++ lab03.py
@@ -0,0 +1,24 @@
+"""
+Welcome to Lab 03. This is Brendan speaking.
+
+We WILL be grading for Header, Comments, Formatting.
+
+Follow the instructions in the lab document. <---- very important
+Write your code to complete the tests in the Gradescope autograder.
+
+Necessary print statements:
+
+"Please select one of the following to convert to meters: cm m km in ft: "
+"Please select one of the following to convert to grams: mg g kg lbs: "
+"Please select one of the following to convert to meters per second: m/s km/h ft/s mph: "
+"Please select one of the following to convert to Celsius: C F K: "
+
+"Please input a value: "
+"Unsupported unit"
+"Invalid unit type"
+
+"""
+
+print("Welcome to the SI units calculator!")
+
+print("Please input a type of unit that you would like to convert. Here are your options: \ndistance \nmass \nspeed \ntemperature\n")
