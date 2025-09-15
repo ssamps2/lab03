ac03504ff72da47f9168d2521537c24976971779
ssamps2 <ssamps2@clemson.edu>
2025-09-15 10:18:43 -0400

    resubmit #3

--- lab03.py
+++ lab03.py
@@ -26,7 +26,7 @@ Assignment: Lab 03
 Course: CPSC 1051
 Lab Section: 002
 
-This program will convert given units to their SI equivalents for distance, mass, speed, and temperature.
+This program will convert inputed units to their SI equivalents for distance, mass, speed, and temperature.
 
 """
 #welcome user
@@ -34,93 +34,122 @@ print("Welcome to the SI units calculator!")
 
 #ask for type of unit
 print("Please input a type of unit that you would like to convert. Here are your options: \ndistance \nmass \nspeed \ntemperature\n")
-type = str(input())     #user chooses type of unit: distance, mass, speed, temp
+type = str(input().strip())     #user chooses type of unit: distance, mass, speed, temp
 
 #user asks for distance
 if type == "distance":
     print("Please select one of the following to convert to meters: cm m km in ft: ") 
-    unit_distance = str(input())        #user chooses a unit within distance
+    unit_distance = str(input().strip())        #user chooses a unit within distance
     print("Please input a value: ")
-    value_distance = float(input())     #user inputs a value for conversion
-    if unit_distance == "cm":       #cm conversion
-        cm = value_distance / 100
-        print(f"{value_distance:.2f} cm in meters: {cm:.2f}")
-    elif unit_distance == "m":      #m conversion
-        m = value_distance
-        print(f"{value_distance:.2f} m in meters: {m:.2f}")
-    elif unit_distance == "km":     #km conversion
-        km = value_distance * 1000
-        print(f"{value_distance:.2f} km in meters: {km:.2f}")
-    elif unit_distance == "in":     #in conversion
-        inch = value_distance * 0.0254
-        print(f"{value_distance:.2f} in in meters: {inch:.2f}")
-    elif unit_distance == "ft":     #ft conversion
-        ft = value_distance * 0.3048
-        print(f"{value_distance:.2f} ft in meters: {ft:.2f}")
-    else:
+
+    if unit_distance not in ["cm", "m", "km", "in", "ft"]:
         print("Unsupported unit")
+   
+    else:
+        value_distance = float(input().strip())     #user inputs a value for conversion
+        
+        if unit_distance == "cm":       #cm conversion
+            cm = value_distance / 100
+            print(f"{value_distance:.2f} cm in meters: {cm:.2f}")
+       
+        elif unit_distance == "m":      #m conversion
+            m = value_distance
+            print(f"{value_distance:.2f} m in meters: {m:.2f}")
+        
+        elif unit_distance == "km":     #km conversion
+            km = value_distance * 1000
+            print(f"{value_distance:.2f} km in meters: {km:.2f}")
+       
+        elif unit_distance == "in":     #in conversion
+            inch = value_distance * 0.0254
+            print(f"{value_distance:.2f} in in meters: {inch:.2f}")
+        
+        else:     #ft conversion
+            ft = value_distance * 0.3048
+            print(f"{value_distance:.2f} ft in meters: {ft:.2f}")
 
 #user asks for mass
 elif type == "mass":
     print("Please select one of the following to convert to grams: mg g kg lbs: ") 
-    unit_mass = str(input())        #user chooses unit within mass
+    unit_mass = str(input().strip())        #user chooses unit within mass
     print("Please input a value: ")
-    value_mass = float(input())     #user inputs a value for conversion
-    if value_mass < 0:         #no negative mass
-        print(f"You can't have a negative mass!")
-    elif unit_mass == "mg":       #mg conversion
-        mg = value_mass / 1000
-        print(f"{value_mass:.2f} mg in grams: {mg:.2f}")
-    elif unit_mass == "g":      #g conversion
-        g = value_mass
-        print(f"{value_mass:.2f} g in grams: {g:.2f}")
-    elif unit_mass == "kg":     #kg conversion
-        kg = value_mass * 1000
-        print(f"{value_mass:.2f} kg in grams: {kg:.2f}")
-    elif unit_mass == "lbs":        #lbs conversion
-        lbs = value_mass * 453.592
-        print(f"{value_mass:.2f} lbs in grams: {lbs:.2f}")
-    else:
+   
+    if unit_mass not in ["mg", "g", "kg", "lbs"]:
         print("Unsupported unit")
+   
+    else:
+        value_mass = float(input().strip())     #user inputs a value for conversion
+       
+        if value_mass < 0:         #no negative mass
+            print(f"You can't have a negative mass!")
+       
+        elif unit_mass == "mg":       #mg conversion
+            mg = value_mass / 1000
+            print(f"{value_mass:.2f} mg in grams: {mg:.2f}")
+       
+        elif unit_mass == "g":      #g conversion
+            g = value_mass
+            print(f"{value_mass:.2f} g in grams: {g:.2f}")
+       
+        elif unit_mass == "kg":     #kg conversion
+            kg = value_mass * 1000
+            print(f"{value_mass:.2f} kg in grams: {kg:.2f}")
+       
+        else:        #lbs conversion
+            lbs = value_mass * 453.592
+            print(f"{value_mass:.2f} lbs in grams: {lbs:.2f}")
 
 #user asks for speed
 elif type == "speed":
     print("Please select one of the following to convert to meters per second: m/s km/h ft/s mph: ") 
-    unit_speed = str(input())       #user chooses unit within speed
+    unit_speed = str(input().strip())       #user chooses unit within speed
     print("Please input a value: ")
-    value_speed = float(input())        #user inputs a value for conversion
-    if unit_speed == "m/s":         #m/s conversion
-        ms = value_speed
-        print(f"{value_speed:.2f} m/s in meters per second: {ms:.2f}")
-    elif unit_speed == "km/h":      #km/h conversion
-        kmh = value_speed * 0.277778
-        print(f"{value_speed:.2f} km/h in meters per second: {kmh:.2f}")
-    elif unit_speed == "ft/s":       #ft/s conversion
-        fts = value_speed * 0.3048
-        print(f"{value_speed:.2f} ft/s in meters per second: {fts:.2f}")
-    elif unit_speed == "mph":        #mph conversion
-        mph = value_speed * 0.44704
-        print(f"{value_speed:.2f} mph in meters per second: {mph:.2f}")
-    else:
+   
+    if unit_speed not in ["m/s", "km/h", "ft/s", "mph"]:
         print("Unsupported unit")
+   
+    else:
+        value_speed = float(input().strip())        #user inputs a value for conversion
+       
+        if unit_speed == "m/s":         #m/s conversion
+            ms = value_speed
+            print(f"{value_speed:.2f} m/s in meters per second: {ms:.2f}")
+       
+        elif unit_speed == "km/h":      #km/h conversion
+            kmh = value_speed * 0.277778
+            print(f"{value_speed:.2f} km/h in meters per second: {kmh:.2f}")
+       
+        elif unit_speed == "ft/s":       #ft/s conversion
+            fts = value_speed * 0.3048
+            print(f"{value_speed:.2f} ft/s in meters per second: {fts:.2f}")
+        
+        else:        #mph conversion
+            mph = value_speed * 0.44704
+            print(f"{value_speed:.2f} mph in meters per second: {mph:.2f}")
 
 #user asks for temp
 elif type == "temperature":
     print("Please select one of the following to convert to Celsius: C F K: ") 
-    unit_temp = str(input())        #user chooses unit within temp
+    unit_temp = str(input().strip())        #user chooses unit within temp
     print("Please input a value: ")
-    value_temp = float(input())     #user inputs a value for conversion
-    if unit_temp == "C":        #C conversion
-        cel = value_temp
-        print(f"{value_temp:.2f} C in Celsius: {cel:.2f}")
-    elif unit_temp == "F":      #F conversion
-        far = (value_temp - 32) * 5/9
-        print(f"{value_temp:.2f} F in Celsius: {far:.2f}")
-    elif unit_temp == "K":      #K conversion
-        kal = value_temp - 273.15
-        print(f"{value_temp:.2f} K in Celsius: {kal:.2f}")
-    else:
+   
+    if unit_temp not in ["C", "F", "K"]:
         print("Unsupported unit")
+   
+    else:
+        value_temp = float(input().strip())     #user inputs a value for conversion
+       
+        if unit_temp == "C":        #C conversion
+            cel = value_temp
+            print(f"{value_temp:.2f} C in Celsius: {cel:.2f}")
+        
+        elif unit_temp == "F":      #F conversion
+            far = (value_temp - 32) * 5/9
+            print(f"{value_temp:.2f} F in Celsius: {far:.2f}")
+       
+        else:      #K conversion
+            kal = value_temp - 273.15
+            print(f"{value_temp:.2f} K in Celsius: {kal:.2f}")
 
 else:
     print("Invalid unit type")
